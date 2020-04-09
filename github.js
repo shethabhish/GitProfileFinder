237b30ac7c8786d338104894d6b3f829e78cc19a
shethabhish <shethabhish@gmail.com>
2020-04-09 15:25:24 -0700

    Initial commit

---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = '64534e7c0e3edb3fb860';
+    this.client_secret = 'f9f8f49ea04e259b7479b875d43ca643a54cbd45';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
